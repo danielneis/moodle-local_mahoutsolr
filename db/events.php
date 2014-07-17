c8676f762bfba425ba28e79430a162faf488f68b
Daniel Neis Araujo <danielneis@gmail.com>
2014-07-17 18:43:36 -0300

    Moved the related discussions list to MDL-46429

--- db/events.php
+++ db/events.php
@@ -6,8 +6,4 @@ $observers = array(
         'eventname'   => '\mod_forum\event\post_created',
         'callback'    => '\local_solr\forum_observers::post_created',
     ),
-    array(
-        'eventname'   => '\mod_forum\event\discussion_viewed',
-        'callback'    => '\local_solr\forum_observers::discussion_viewed',
-    ),
 );
